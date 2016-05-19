8905fb247a721ef8fdda3e3f4b2ed171380cd178
Faxn <blake.curry@gmx.us>
2016-05-19 11:47:00 -0400

    Layout of dev-board page

--- src/chessPieces.js
+++ src/chessPieces.js
@@ -1,39 +1,56 @@
 
-exports.white = 0;
-exports.black = 128;
+define(function(){
+    var exports = {};
+    
+    //utility
+    exports.empty=0;
+    
+    //Sides
+    exports.white = 0;
+    exports.black = 128;
 
-exports.pawn = 1;
-exports.knight = 2;
-exports.bishop = 3;
-exports.rook = 4;
-exports.king = 5;
-exports.queen = 6;
-
-unicode = {
-    1:'\u2659',
-    2:'\u2658',
-    3:'\u2657',
-    4:'\u2656',
-    5:'\u2654',
-    6:'\u2655',
-    129:'\u265f',
-    130:'\u265e',
-    131:'\u265d',
-    132:'\u265c',
-    133:'\u265a',
-    134:'\u265b',
-}
-
-
-offset = 6;
-exports.toUnicode= function(piece){
-    if(piece == 0){
-        
-	return ' '
+    //Chess Pieces
+    exports.pawn = 1;
+    exports.knight = 2;
+    exports.bishop = 3;
+    exports.rook = 4;
+    exports.king = 5;
+    exports.queen = 6;
+    
+    exports.types = [1,2,3,4,5,6];
+    values = [];
+    for(i in exports.types){
+        values[values.length] = parseInt(i);
+        values[values.length] = parseInt(i)+128;
     }
-    piece = unicode[piece]
+    exports.values = values;
 
-    return piece;
-}
-//meaningless comment
+    unicode = {
+        1:'\u2659',
+        2:'\u2658',
+        3:'\u2657',
+        4:'\u2656',
+        5:'\u2654',
+        6:'\u2655',
+        129:'\u265f',
+        130:'\u265e',
+        131:'\u265d',
+        132:'\u265c',
+        133:'\u265a',
+        134:'\u265b',
+    }
 
+
+    offset = 6;
+    exports.toUnicode= function(piece){
+        if(piece == 0){
+            
+        return ' '
+        }
+        piece = unicode[piece]
+
+        return piece;
+    }
+    
+    return exports;
+});
